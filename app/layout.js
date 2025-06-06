428f4da51f2073e4e6ecb592ac2b995e68b74355
Chethan T <chethant91@gmail.com>
2025-06-06 11:25:35 +0530

    setup the app

---
+++ app/layout.js
@@ -0,0 +1,41 @@
+import {Outfit } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/Header";
+import { Toaster } from "sonner";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Doctor Appointment",
+  description: "Book and manage your doctor appointments easily.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${outfit.className}`}
+      >
+     <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with 💗 by Chethan</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+      </body>
+    </html>
+  );
+}
